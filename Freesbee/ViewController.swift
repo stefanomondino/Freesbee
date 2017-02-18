1bb759005dcba8898c3c32222aee2d1ba4ea40ab
Stefano Mondino <stefano.mondino.dev@gmail.com>
2017-02-18 18:01:48 +0100

    Initial Commit

---
+++ Freesbee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Freesbee
+//
+//  Created by Stefano Mondino on 18/02/17.
+//  Copyright © 2017 Synesthesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
